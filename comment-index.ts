dbbced75516b4bef3a433a507e9441d14546adc3
Anders Olav Candasamy <anders.can1@gmail.com>
2021-07-28 18:08:09 +0200

    test comment

---
+++ comment-index.ts
@@ -0,0 +1,32 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run() {
+  try {
+    const message = core.getInput("message");
+    const github_token = core.getInput("GITHUB_TOKEN");
+
+    const context = github.context;
+    const pull_request_number = context.issue.number;
+    console.log(JSON.stringify(context, null, 2));
+    if (pull_request_number == null) {
+      core.setFailed("No pull_request_number found.");
+      return;
+    }
+    console.log({ pull_request_number });
+    const octokit = github.getOctokit(github_token);
+    const new_comment = octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request_number,
+      body: message,
+    });
+    await new_comment;
+    core.setOutput("time", "time-output");
+
+    console.log(JSON.stringify(new_comment, null, 2));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
