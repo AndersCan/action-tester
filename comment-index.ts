a829042ca068c4aa1f7e621f56cd8d536f42e997
Anders Olav Candasamy <anders.can1@gmail.com>
2021-07-29 10:32:12 +0200

    update

--- comment-index.ts
+++ comment-index.ts
@@ -1,38 +1,59 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-async function run() {
+main();
+
+async function main() {
   try {
-    const message = core.getInput("message");
+    const message = core.getInput("comment-message", { required: true });
     const github_token = core.getInput("github-token", { required: true });
-    console.log("token length", github_token.length);
+    const requiredPullRequestUser = core.getInput("required-pr-creator", {
+      required: true,
+    });
+    console.log(JSON.stringify(github.context, null, 2));
+
+    const { pull_request: pr } = github.context.payload;
+    if (github_token.length !== 40) {
+      core.warning(
+        `github_token is not expected length 40. Was ${github_token.length}`
+      );
+    }
     const context = github.context;
     const pull_request_number = context.issue.number;
-    console.log(JSON.stringify(context, null, 2));
+
     if (pull_request_number == null) {
-      core.setFailed("No pull_request_number found.");
+      core.setFailed(
+        "No pull_request_number found. Must be run on `pull_request`"
+      );
       return;
     }
-    console.log({ pull_request_number });
+
+    if (requiredPullRequestUser !== "*") {
+      const isRequiredUser = pr.user.login === requiredPullRequestUser;
+      if (!isRequiredUser) {
+        const notRequiredPrCreatorMessage = `'${pr.user.login}' does not match required-pr-creator '${requiredPullRequestUser}'`;
+
+        core.info(notRequiredPrCreatorMessage);
+        core.setOutput("result", notRequiredPrCreatorMessage);
+
+        return;
+      }
+    }
+
+    core.info(`current pullrequest: ${pull_request_number}`);
+
     const octokit = github.getOctokit(github_token);
     const new_comment = octokit.rest.issues.createComment({
       ...context.repo,
       issue_number: pull_request_number,
       body: message,
     });
-
-    await sleep(100);
     await new_comment;
-    core.setOutput("time", "time-output");
-
+    core.setOutput("success", true);
+    core.setOutput("result", "successfully commented");
     console.log(JSON.stringify(new_comment, null, 2));
   } catch (error) {
+    core.setOutput("result", `Error: ${error.message}`);
     core.setFailed(error.message);
   }
 }
-function sleep(ms) {
-  return new Promise((res) => {
-    setTimeout(res, ms);
-  });
-}
-run();
